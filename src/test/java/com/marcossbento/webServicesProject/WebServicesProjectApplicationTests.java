bec0263852d0d23340bed22ba815e76167a0684a
marcossbento <marcossbento14@gmail.com>
2023-10-04 18:46:27 -0300

    Creating Project

---
+++ src/test/java/com/marcossbento/webServicesProject/WebServicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcossbento.webServicesProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
