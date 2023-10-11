1e097d3a2c4b6bde3af173421cbc3a6a589456fd
marcossbento <marcossbento14@gmail.com>
2023-10-11 19:15:30 -0300

    Service layer and component registration

---
+++ src/main/java/com/marcossbento/webServicesProject/services/UserService.java
@@ -0,0 +1,26 @@
+package com.marcossbento.webServicesProject.services;
+
+import com.marcossbento.webServicesProject.entities.User;
+import com.marcossbento.webServicesProject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
